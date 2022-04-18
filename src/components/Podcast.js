d069fceb54ca10cffc1d19d49be844053968f313
journo10 <akifbgtr@gmail.com>
2022-04-18 20:27:25 +0300

    yazım hatası düzeltildi.

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { PodcastTheme } from '../contexts/Podcast'
+import { PodcastTheme } from '../contexts/PodcastContext'
 
 
 const Podcast = () => {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { HomeProvider } from "./contexts/HomeContext";
 import { VideoProvider } from "./contexts/VideoContext";
-import { PodcastProvider } from "./contexts/Podcast";
+import { PodcastProvider } from "./contexts/PodcastContext";
 
 //REACT 18
 const root = createRoot(document.getElementById("root"));
